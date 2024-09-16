4d0f170d1083f1f79353f56c7884e5efbeb6c878
Ash@metaschool <ash20pk@gmail.com>
2024-09-16 17:16:12 +0800

    Fixed theme issues

--- interface/src/theme.js
+++ interface/src/theme.js
@@ -1,18 +1,31 @@
-import { extendTheme } from "@chakra-ui/react"
+import { extendTheme } from "@chakra-ui/react";
+
+const config = {
+  initialColorMode: "light", // Default mode
+  useSystemColorMode: false, // Use system preference
+};
+
+const colors = {
+  light: {
+    bg: "gray.100",
+    text: "black",
+  },
+  dark: {
+    bg: "gray.800",
+    text: "white",
+  },
+};
 
 const theme = extendTheme({
-  config: {
-    initialColorMode: "dark",
-    useSystemColorMode: true,
-  },
+  config,
   styles: {
-    global: {
+    global: (props) => ({
       body: {
-        bg: "gray.50",
-        color: "gray.800",
+        bg: props.colorMode === "dark" ? colors.dark.bg : colors.light.bg,
+        color: props.colorMode === "dark" ? colors.dark.text : colors.light.text,
       },
-    },
+    }),
   },
-})
+});
 
-export default theme
+export default theme;
